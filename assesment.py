c36a47d4e4469cc5eba7f67bf63a5b7f80b11403
rutujakardile1998 <59498764+rutujakardile1998@users.noreply.github.com>
2021-10-18 10:36:10 +0530

    added function for specific country

--- assesment.py
+++ assesment.py
@@ -2,10 +2,13 @@
 """
 Created on Sun Oct 17 19:49:01 2021
 
-@author: Kardile
+@author: Rutuja Kardile
 """
 
 import cx_Oracle
+import pandas as pd
+
+
 con=cx_Oracle.connect('system/123@127.0.0.1/XE')
 if con!=None:
     print(con.version)
@@ -14,6 +17,28 @@ if con!=None:
 cur=con.cursor()
 
 cur.execute("select * from patients")
-for row in cur.fetchall():
-    print(row)
-#table_rows = cur.fetchall()
+table_rows = cur.fetchall()
+df = pd.read_sql('SELECT * FROM patients',con=con)  # fitting into pandas dataframe
+df.set_index(['CUST_ID'], inplace=True)  # setting default index
+print (df)
+a='IND'.ljust(5)
+ans = df.loc[df['COUNTRY']==a]
+print(ans)
+
+
+def show_data(country):
+    a=country.ljust(5)
+    data = df.loc[df['COUNTRY'] == a]
+    print(data)
+
+def get_file(country):
+    a=country.ljust(5)
+    data = df.loc[df['COUNTRY'] == a]
+    file_name = str(country)
+    data.to_csv('C:/Users/kardi/downloads/' + country + ".csv")  # replace path with your desired path
+    print("File has been created to the specified path")
+
+#calling functions
+
+show_data("IND")
+get_file("IND")
